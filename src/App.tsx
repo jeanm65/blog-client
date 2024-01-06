8a8120cfae55d56b217ab536c064439164ad022b
JeanM65 <jm.randrianant65@gmail.com>
2024-01-06 12:15:19 +0800

    override styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { Theme, useTheme, css } from "@emotion/react";
-import { Button, ThemeProvider, createTheme } from "@mui/material"
+import { Theme, useTheme } from "@emotion/react";
+import { Button, ThemeProvider, Typography, createTheme } from "@mui/material"
 import { ThemeProvider as EmotionThemeProvider} from '@emotion/react'
 import { green, purple } from '@mui/material/colors';
 import { GlobalStyles } from "./GlobalStyles";
+import { css } from "@emotion/css";
 
 const theme = createTheme({
   palette: {
@@ -19,18 +20,39 @@ const theme = createTheme({
 
 
 const classes = {
-  button: (theme: Theme) => ({
+  DefaultButton: (theme: Theme) => ({
     backgroundColor: theme.palette.secondary.dark
   })
+}
+
+type Props = {
+  className?: string;
+  rooClassName?: string;
+  textClassName?: string
+}
+const ActiveButton = ({ className, rooClassName, textClassName } : Props) => {
+  console.log('rooClassName:', rooClassName);
+  console.log('className:', className);
+  return (
+    <div className={ rooClassName }>
+      <Button variant="contained" css={ classes.DefaultButton } className={className}>Button emotion</Button>
+      <Typography className={ textClassName }>Hello</Typography>
+    </div>  
+  )
 }
 
 const Home = () => {
   const appTheme = useTheme();
   return (
-      <>
+      <div css={{ minHeight: '100vh'}} className= 'flexColumn'>
+        <ActiveButton />
+        <ActiveButton css={{ backgroundColor:'#000', color:'#fff' }} 
+                      rooClassName={css({ backgroundColor: 'red', paddingTop: 4, paddingBottom: 4 })}
+                      textClassName={css({ color: 'blue' })}>
+        </ActiveButton>
        <Button variant="contained" sx={{ bgcolor:appTheme.palette.secondary.main }}>Button sx</Button>
-       <Button variant="contained" css={{ backgroundColor:classes.button }}>Button emotion</Button>
-      </>
+       <Button variant="contained" className='buttonCss2'>Button emotion</Button>
+      </div>
      
   )
 }
