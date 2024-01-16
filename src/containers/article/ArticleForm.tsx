27e4c628ecce11e4418ce0ae0152f057902523c3
JeanM65 <jm.randrianant65@gmail.com>
2024-01-16 16:47:11 +0800

    refactor textfield

--- src/containers/article/ArticleForm.tsx
+++ src/containers/article/ArticleForm.tsx
@@ -1,6 +1,7 @@
 import { IArticle, IArticleInput } from "../../types/article.type";
-import { Controller, SubmitHandler, useForm  } from "react-hook-form";
-import { Button, Stack, TextField } from "@mui/material";
+import TextField from "../../components/form/textfield/TextField";
+import { SubmitHandler, useForm, FormProvider  } from "react-hook-form";
+import { Button, Stack } from "@mui/material";
 import { useEffect } from "react";
 
 type Props = {
@@ -15,9 +16,11 @@ const initialValues = {
 }
 
 const ArticleForm = ( { article, onSubmit, loading }: Props) => {
-  const { control, handleSubmit, reset } = useForm<IArticleInput>({
+  const form  = useForm<IArticleInput>({
     defaultValues: initialValues 
   });
+
+  const { handleSubmit, reset }= form;
 
   useEffect(() => {
     if(!article) return;
@@ -25,43 +28,34 @@ const ArticleForm = ( { article, onSubmit, loading }: Props) => {
       title: article.title,
       content: article.content
     })
-  }, [article, reset]);
+  }, [article, reset]); 
 
   const _onSubmit: SubmitHandler<IArticleInput> = (values) => {
     onSubmit(values);
+    reset(initialValues);
   }
   return (
-      <form onSubmit={handleSubmit(_onSubmit)}>
-        <Stack spacing={2}>
-            <Controller
-              name="title"
-              control ={control}
-              render = {({ field }) => (
-              <TextField 
+    <FormProvider { ...form }>
+        <form onSubmit={handleSubmit(_onSubmit)}>
+          <Stack spacing={2}>
+              <TextField
                 label="title" 
-                variant="outlined" 
-                { ...field } 
+                name="title"
               /> 
-           )}
-         />
-              <Controller
-                    name="content"
-                    control ={control}
-                    render={({ field }) => (
-                    <TextField
-                        label="content"
-                        multiline
-                        maxRows={4}
-                        variant="outlined"
-                        { ... field }
-                    />
-            )}
-                 />  
-        <Button type="submit" variant="contained">
-          { loading ? "...loading" : "Save"}
-        </Button>
-        </Stack>
+              <TextField
+                label="content"
+                name="content"
+                multiline
+                maxRows={4}
+              />
+                     
+          <Button type="submit" variant="contained">
+            { loading ? "...loading" : "Save"}
+          </Button>
+          </Stack>
       </form>
+    </FormProvider>
+      
   );
 };
 
